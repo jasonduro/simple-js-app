585edc391382fb156ede40da72839d02ba12b022
Jason <product@jasonlamanque.com>
2023-01-06 10:50:00 +0100

    added search functionality code
    tried to add search functionality to the navbar search item via js code. need to make some updates still

--- js/scripts.js
+++ js/scripts.js
@@ -28,7 +28,6 @@ let pokemonRepository = (function() {
         });
     }
 
-
     //function to load pokemon API List
     function loadList() {
         return fetch(apiUrl).then(function (response) {
@@ -54,14 +53,19 @@ let pokemonRepository = (function() {
         }).then(function (details) {
             item.imageUrl = details.sprites.front_default;
             item.height = details.height;
-            item.types = details.types;
             item.weight = details.weight;
-            item.abilities = details.abilities;
+            item.types = [];
+            for (let i = 0; i < details.types.length; i++){
+                item.types.push(details.types[i].type.name);
+            }
+            item.abilities = [];
+            for (let i = 0; i < details.abilities.length; i++){
+                item.abilities.push(details.abilities[i].ability.name);
+            }
         }).catch(function (e) {
             console.error(e);
         });        
     }
-
 
     //function to show details
     function showDetails(pokemon) {
@@ -106,3 +110,37 @@ pokemonRepository.loadList().then(function() {
         pokemonRepository.addListItem(pokemon);
     });
 });
+
+    //search functionality
+    let filter = document.querySelector('#searchBar');
+    let noResults = document.createElement('h3');
+    noResults.innerText = 'No Pokémon found.';
+
+    filter.addEventListener('input', () => {
+
+    let list = document.querySelector('.pokemon-list');
+
+    let value = filter.value.toLowerCase();
+    listItems = list.getElementsByTagName('li');
+    let isPokemonFound = false; // Help to control the no result message
+
+    for (let i = 0; i < listItems.length; i++) {
+    let button = listItems[i].getElementsByTagName('button')[0];
+    let pokemon = button.textContent || button.innerText;
+
+    if (pokemon.toLowerCase().indexOf(value) > -1) {
+    listItems[i].style.display = "";
+    isPokemonFound = true;
+    } else {
+    listItems[i].style.display = "none";
+    }
+    }
+
+    if (!isPokemonFound) {
+    list.appendChild(noResults);
+    }
+
+    if (list.contains(noResults) && isPokemonFound) {
+    list.removeChild(noResults)
+    }
+    });
